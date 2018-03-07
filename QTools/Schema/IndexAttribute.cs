0f5e117947efc3bcc05439b528f95d5fe4f3dc67
machonky <dev@macrobert.com>
2018-03-07 22:49:38 +0800

    IndexAttribute added

---
+++ QTools/Schema/IndexAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QTools.Schema
+{
+    public class IndexAttribute : Attribute
+    {
+        public IndexAttribute(int indexGroupId)
+        {
+            IndexGroupId = indexGroupId;
+        }
+
+        public int IndexGroupId { get; }
+    }
+}
