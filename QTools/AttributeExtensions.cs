dc13673e6b53eb17af4f62412c5a2cd69500c0a3
machonky <martin@macrobert.com>
2018-03-16 22:34:12 +0800

    Stop polluting external namespaces

--- QTools/AttributeExtensions.cs
+++ QTools/AttributeExtensions.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 namespace QTools
 {
-    public static class AttributeExtensions
+    internal static class AttributeExtensions
     {
         public static bool HasAttribute<T>(this MemberInfo info) where T : Attribute => info.GetCustomAttribute<T>() != null;
     }
